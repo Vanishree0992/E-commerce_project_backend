e8c75cf5d22ddcf91e8ed9111ce356b6b9c5d0e0
Vanishree0992 <vani0992@gmail.com>
2025-09-24 23:08:52 +0530

    Fix STATIC_ROOT for Render

--- backend/settings.py
+++ backend/settings.py
@@ -132,10 +132,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # Static & Media
-STATIC_URL = "/static/"
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
+
+# Optional: if you want to serve media files too
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
